af02efdd8ee5ec30b0a1ed69fbbc73c7d18a1442
suer <sue@nagoya-u.jp>
2010-12-26 03:45:23 +0900

    ダイナミックリンクライブラリのロードを移動

--- R/sdbin4.R
+++ R/sdbin4.R
@@ -1,9 +1,3 @@
-.onLoad <- function(lib, pkg) {
-  library.dynam("command_wrapper.so", pkg, lib)
-  library.dynam("SapidAR4Wrapper.so", pkg, lib)
-  library.dynam("RSapid.so", pkg, lib)
-}
-
 sdb4 <- function(filelist, sdbDirectory="SDB", targetProgramName="a.out") {
   command <- paste("-sdbd", sdbDirectory, "-o", targetProgramName, sep=" ")
   files <- ""
